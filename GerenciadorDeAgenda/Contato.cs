d3bf80e515ab315907150ede7fb627fa1e76188f
AlexandreLopes02 <alenovoxio@gmail.com>
2025-11-11 09:42:18 -0300

    feat: implementar classe Contato

--- GerenciadorDeAgenda/Contato.cs
+++ GerenciadorDeAgenda/Contato.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
-namespace GerenciadorDeAgenda
+namespace GerenciadorDeAgenda;
+
+internal class Contato
 {
-    internal class Contato
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Telefone { get; set; }
+
+    public override string ToString()
     {
+        return $"ID: {Id} - Nome: {Nome}, Telefone: {Telefone}";
     }
+
 }
